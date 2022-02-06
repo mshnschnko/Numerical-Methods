f3123cc8a1502faa9dd567ca69711ec4f79a8011
mshnschnko <misha321654987i@gmail.com>
2022-02-07 02:02:13 +0300

    research for the grid type and count of nodes are done

--- LAB_2_1/Lagrange.cpp
+++ LAB_2_1/Lagrange.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <math.h>
+#define MATH_USE_DEFINES
+#include <cmath>
 using namespace std;
 
 double f(double x) {
-    return (x * x - sin(x)-0.25);
+    return (x * x * cos(2*x) + 1);
+    //return (x * x - sin(x) - 0.25);
 }
 
 double LagrangePolynom (const double* nodes, const double* y, double x, int nodesCount) {
@@ -21,22 +23,50 @@ double LagrangePolynom (const double* nodes, const double* y, double x, int node
     return sum;
 }
 
-int main() {
-    int nodesCount = 4;
-    double a = 0.0, b = 2.0;
-    auto* x = new double [nodesCount + 1];
-    auto* y = new double [nodesCount + 1];
-    for (int i = 0; i <= nodesCount; i++) {
-        x[i] = a + i * ((b - a) / (double)nodesCount);
-        y[i] = f(x[i]);
+void FillFile(FILE* file, double a, double b, const double* nodes, const double* y, int nodesCount) {
+
+    for (int i = 0; i <= 49; i++) {
+        double xx = a + i * ((b - a) / 49.0);
+        double yy = LagrangePolynom(nodes, y, xx, nodesCount);
+        fprintf(file, "%.17lf;%.17lf\n", xx, yy);
     }
-    const char* tableValuesFilename = "TableValues.csv";
-    FILE* tableValues = fopen(tableValuesFilename, "w");
-    FillFile(tableValues, )
-    cout << LagrangePolynom(x, y, 1.8, nodesCount);
-/*    for (int i = 0; i <= nodesCount; i++) {
-        cout << x[i] << endl;
-        cout << y[i] << endl;
-    }*/
+}
+
+int main() {
+    const char *tableValuesUniformFilename = "TableValuesUniformGrid.csv";
+    FILE *tableValuesUniform = fopen(tableValuesUniformFilename, "w");
+    if (tableValuesUniform == nullptr)
+        return 0;
+    const char *tableValuesChebFilename = "TableValuesChebGrid.csv";
+    FILE *tableValuesCheb = fopen(tableValuesChebFilename, "w");
+    if (tableValuesCheb == nullptr) {
+        fclose(tableValuesUniform);
+        return 0;
+    }
+    double a = -1.0, b = 2.5;
+    for (int nodesCount = 5; nodesCount <= 60; nodesCount+=5) {
+        auto *x = new double[nodesCount + 1];
+        auto *y = new double[nodesCount + 1];
+        for (int i = 0; i <= nodesCount; i++) {
+            x[i] = a + i * ((b - a) / (double) nodesCount);
+            //printf("%.17lf\n", x[i]);
+            y[i] = f(x[i]);
+        }
+        FillFile(tableValuesUniform, a, b, x, y, nodesCount);
+
+        for (int i = 0; i < nodesCount; i++) {
+            //x[i] = cos(M_PI * (2 * i + 1) / (2 * nodesCount));
+            x[i] = (b - a) * cos(M_PI * (2 * i + 1) / (2 * nodesCount)) / 2 + (a + b) / 2;
+            //printf("%.17lf\n", x[i]);
+            //cout << x[i] << endl;
+            y[i] = f(x[i]);
+            //cout << y[i] << endl;
+        }
+        FillFile(tableValuesCheb, a, b, x, y, nodesCount - 1);
+        delete[] x;
+        delete[] y;
+    }
+    fclose(tableValuesUniform);
+    fclose(tableValuesCheb);
     return 0;
 }
