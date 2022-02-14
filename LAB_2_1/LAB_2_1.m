5988c17bfc99796bfe874937b4582d1abd95535b
mshnschnko <misha321654987i@gmail.com>
2022-02-14 20:18:40 +0300

    extra formula for uniform grid

--- LAB_2_1/LAB_2_1.m
+++ LAB_2_1/LAB_2_1.m
@@ -6,6 +6,10 @@ data2 = importdata("cmake-build-debug/TableValuesChebGrid.csv");
 x2 = data2(:,1);
 y2 = data2(:,2);
 
+data3 = importdata("cmake-build-debug/TableValuesUniSpecGrid.csv");
+x3 = data3(:,1);
+y3 = data3(:,2);
+
 %f = @(x) x.^2-sin(x)-0.25;
 %f = @(x) x.^3 - exp(x) + 1;
 f = @(x) x.^2.*cos(2*x) + 1;
@@ -13,6 +17,7 @@ a = -2.0;
 b = 2.0;
 i = 0;
 
+% %============================ графики функций =============================
 % figure
 % % for n = 3:2:7
 %     fplot(f, [a, b], 'LineWidth', 3);
@@ -28,6 +33,23 @@ i = 0;
 % legend('function', '4 nodes', '6 nodes', '8 nodes')
 % title('Uniform grid')
 % i = 0;
+% 
+% figure
+% % for n = 3:2:7
+%     fplot(f, [a, b], 'LineWidth', 3);
+%     hold all
+%     grid on
+%     plot(x3(i*100+2:(i+1)*100-1, :), y3(i*100+2:(i+1)*100-1, :));
+%     i = i + 1;
+%     plot(x3(i*100+2:(i+1)*100-1, :), y3(i*100+2:(i+1)*100-1, :));
+%     i = i + 1;
+%     plot(x3(i*100+2:(i+1)*100-1, :), y3(i*100+2:(i+1)*100-1, :), 'g');
+%     i = i + 1;
+% % end
+% legend('function', '4 nodes', '6 nodes', '8 nodes')
+% title('Uniform Spec grid')
+% i = 0;
+% 
 % figure
 % % for n = 3:2:7
 %     fplot(f, [a, b], 'LineWidth', 3);
@@ -42,9 +64,9 @@ i = 0;
 % % end
 % legend('function', '4 nodes', '6 nodes', '8 nodes')
 % title('Chebyshev grid')
-% 
 % i = 0;
 % 
+% %============================ графики ошибок ==============================
 % figure
 % errorUni = abs(f(x(i*100+1:(i+1)*100, :)) - y(i*100+1:(i+1)*100, :));
 % plot(x(i*100+1:(i+1)*100, :), errorUni)
@@ -58,7 +80,21 @@ i = 0;
 % plot(x(i*100+1:(i+1)*100, :), errorUni)
 % legend('4 nodes', '6 nodes', '8 nodes')
 % title('Uniform error')
+% i = 0;
 % 
+% figure
+% errorUniSpec = abs(f(x3(i*100+2:(i+1)*100-1, :)) - y3(i*100+2:(i+1)*100-1, :));
+% plot(x3(i*100+2:(i+1)*100-1, :), errorUniSpec)
+% hold all
+% grid on
+% i = i + 1;
+% errorUniSpec = abs(f(x3(i*100+2:(i+1)*100-1, :)) - y3(i*100+2:(i+1)*100-1, :));
+% plot(x3(i*100+2:(i+1)*100-1, :), errorUniSpec)
+% i = i + 1;
+% errorUniSpec = abs(f(x3(i*100+2:(i+1)*100-1, :)) - y3(i*100+2:(i+1)*100-1, :));
+% plot(x3(i*100+2:(i+1)*100-1, :), errorUniSpec)
+% legend('4 nodes', '6 nodes', '8 nodes')
+% title('Uniform spec error')
 % i = 0;
 % 
 % figure
@@ -75,56 +111,64 @@ i = 0;
 % legend('4 nodes', '6 nodes', '8 nodes')
 % title('Chebyshev error')
 
-% errorUni = zeros(1,12);
-% errorCheb = zeros(1,12);
-% for i=1:12
-%     errorUni(i) = max(abs(f(x((i-1)*100+1:i*100, :)) - y((i-1)*100+1:i*100, :)));
-%     errorCheb(i) = max(abs(f(x2((i-1)*100+1:i*100, :)) - y2((i-1)*100+1:i*100, :)));
-% end
-% n = linspace(5,60,12);
-% figure
-% semilogy(n+1, errorUni)
-% hold on
-% grid on
-% semilogy(n+1, errorCheb)
-% legend('Uniform', 'Chebyshev')
-% title('Error dependence on the number of nodes')
+%======================= полный график ошибок =============================
+
+errorUni = zeros(1,12);
+errorUniSpec = zeros(1,12);
+errorCheb = zeros(1,12);
+for i=1:12
+    errorUni(i) = max(abs(f(x((i-1)*100+1:i*100, :)) - y((i-1)*100+1:i*100, :)));
+    errorUniSpec(i) = max(abs(f(x3((i-1)*100+2:i*100-1, :)) - y3((i-1)*100+2:i*100-1, :)));
+    errorCheb(i) = max(abs(f(x2((i-1)*100+1:i*100, :)) - y2((i-1)*100+1:i*100, :)));
+end
+n = linspace(5,60,12);
+figure
+semilogy(n+1, errorUni)
+hold on
+grid on
+semilogy(n+1, errorCheb)
+hold on
+semilogy(n+1, errorUniSpec)
+legend('Uniform', 'Chebyshev', 'UniformSpec')
+title('Error dependence on the number of nodes')
 
 
 % =========================== основная прога =============================
-figure
-for n = 5:5:60
-subplot(2,2,1)
-fplot(f, [a, b], 'LineWidth', 3);
-grid on
-hold all
-plot(x(i*100'+1:(i+1)*100', :), y(i*100'+1:(i+1)*100', :), 'or');
-
-plot(x2(i*100'+1:(i+1)*100', :), y2(i*100'+1:(i+1)*100', :), '*g');
-hold off
-legend('function', 'Uniform', 'Chebyshev');
-title(['nodes count = ', num2str(n+1)]);
-
-errorUni = abs(f(x(i*100'+1:(i+1)*100', :)) - y(i*100'+1:(i+1)*100', :));
-errorCheb = abs(f(x2(i*100'+1:(i+1)*100', :)) - y2(i*100'+1:(i+1)*100', :));
-
-subplot(2,2,3)
-semilogy(x(i*100'+1:(i+1)*100', :), errorUni)
-hold on
-grid on
-semilogy(x2(i*100'+1:(i+1)*100', :), errorCheb)
-hold off
-legend('Uniform', 'Chebyshev');
-title('error')
-
-subplot(2,2,[2,4])
-semilogy(n+1, max(errorUni), '^r')
-hold on
-grid on
-semilogy(n+1, max(errorCheb), 'ms')
-legend('Uniform', 'Chebyshev');
-%text(-1.1, 2.5, ['Uniform = ', num2str(max(errorUni))]);
-title(['max error uniform = ', num2str(max(errorUni))], ['max error Cheb = ', num2str(max(errorCheb))])
-i = i + 1;
-pause(0.5);
-end
+% figure
+% for n = 5:5:5
+% %data = linspace(a, b, n);
+% subplot(2,2,1)
+% fplot(f, [a, b], 'LineWidth', 3);
+% grid on
+% hold all
+% plot(x(i*100'+1:(i+1)*100', :), y(i*100'+1:(i+1)*100', :), 'or');
+% 
+% plot(x2(i*100'+1:(i+1)*100', :), y2(i*100'+1:(i+1)*100', :), '*g');
+% %plot(data, f(data));
+% hold off
+% legend('function', 'Uniform', 'Chebyshev');
+% title(['nodes count = ', num2str(n+1)]);
+% 
+% errorUni = abs(f(x(i*100'+1:(i+1)*100', :)) - y(i*100'+1:(i+1)*100', :));
+% errorCheb = abs(f(x2(i*100'+1:(i+1)*100', :)) - y2(i*100'+1:(i+1)*100', :));
+% 
+% subplot(2,2,3)
+% semilogy(x(i*100'+1:(i+1)*100', :), errorUni)
+% hold on
+% grid on
+% semilogy(x2(i*100'+1:(i+1)*100', :), errorCheb)
+% hold off
+% legend('Uniform', 'Chebyshev');
+% title('error')
+% 
+% subplot(2,2,[2,4])
+% semilogy(n+1, max(errorUni), '^r')
+% hold on
+% grid on
+% semilogy(n+1, max(errorCheb), 'ms')
+% legend('Uniform', 'Chebyshev');
+% %text(-1.1, 2.5, ['Uniform = ', num2str(max(errorUni))]);
+% title(['max error uniform = ', num2str(max(errorUni))], ['max error Cheb = ', num2str(max(errorCheb))])
+% i = i + 1;
+% pause(0.5);
+% end
--- LAB_2_1/Lagrange.cpp
+++ LAB_2_1/Lagrange.cpp
@@ -10,7 +10,15 @@ double f(double x) {
     //return (x * x * x - exp(x) + 1);
 }
 
-double LagrangePolynom (const double* nodes, const double* y, double x, int nodesCount) {
+int factorial(int x) {
+    int fact = 1;
+    for (int i = 1; i <= x; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+double LagrangePolynom(const double* nodes, const double* y, double x, int nodesCount) {
     double multiply = 1;
     double sum = 0;
     for (int i = 0; i <= nodesCount; i++) {
@@ -18,6 +26,29 @@ double LagrangePolynom (const double* nodes, const double* y, double x, int node
             if (k != i) {
                 multiply *= (x - nodes[k]) / (nodes[i] - nodes[k]);
             }
+        }
+        sum += y[i] * multiply;
+        multiply = 1;
+    }
+    return sum;
+}
+
+double LagrangeForUniform(const double* y, double x, int nodesCount, double t) {
+    double multiply = 1;
+    double sum = 0;
+    int minus = 0;
+    for (int i = 0; i <= nodesCount; i++) {
+/*        for (int k = 0; k <= nodesCount; k++)
+            multiply *= t - k;
+        if (multiply == 0) {
+            multiply = 1;
+            continue;
+        }*/
+        //minus = (nodesCount - i) % 2 == 0 ? 1 : -1;
+        //sum += y[i] * (minus * multiply) / factorial(i) / factorial(nodesCount - i) / (t - i);
+        for (int k = 0; k <= nodesCount; k++) {
+            if (k != i)
+                multiply *= (t - k)/(i - k);
         }
         sum += y[i] * multiply;
         multiply = 1;
@@ -34,20 +65,37 @@ void FillFile(FILE* file, double a, double b, const double* nodes, const double*
     }
 }
 
+void FillForUniform(FILE* file, double a, double b, const double* y, int nodesCount) {
+    double h = ((b - a) / (double) nodesCount);
+    for (int i = 0; i <= 99; i++) {
+        double xx = a + i * ((b - a) / 99.0);
+        double t = (xx - a) / h;
+        double yy = LagrangeForUniform(y, xx, nodesCount, t);
+        fprintf(file, "%.17lf;%.17lf\n", xx, yy);
+    }
+}
+
 int main() {
     const char *tableValuesUniformFilename = "TableValuesUniformGrid.csv";
-    FILE *tableValuesUniform = fopen(tableValuesUniformFilename, "w");
+    FILE* tableValuesUniform = fopen(tableValuesUniformFilename, "w");
     if (tableValuesUniform == nullptr)
         return 0;
     const char *tableValuesChebFilename = "TableValuesChebGrid.csv";
-    FILE *tableValuesCheb = fopen(tableValuesChebFilename, "w");
+    FILE* tableValuesCheb = fopen(tableValuesChebFilename, "w");
     if (tableValuesCheb == nullptr) {
         fclose(tableValuesUniform);
         return 0;
     }
+    const char *tableValuesUniSpecFilename = "TableValuesUniSpecGrid.csv";
+    FILE* tableValuesUniSpec = fopen(tableValuesUniSpecFilename, "w");
+    if (tableValuesUniSpec == nullptr) {
+        fclose(tableValuesUniform);
+        fclose(tableValuesCheb);
+        return 0;
+    }
     double a = -2.0, b = 2.0;
-//    for (int nodesCount = 3; nodesCount <= 7; nodesCount+=2) {
     for (int nodesCount = 5; nodesCount <= 60; nodesCount+=5) {
+//    for (int nodesCount = 3; nodesCount <= 7; nodesCount+=2) {
             auto *x = new double[nodesCount + 1];
             auto *y = new double[nodesCount + 1];
             for (int i = 0; i <= nodesCount; i++) {
@@ -56,6 +104,7 @@ int main() {
                 y[i] = f(x[i]);
             }
             FillFile(tableValuesUniform, a, b, x, y, nodesCount);
+            FillForUniform(tableValuesUniSpec, a, b, y, nodesCount);
 //            auto *x1 = new double[nodesCount + 1];
 //            auto *y1 = new double[nodesCount + 1];
             for (int i = 0; i <= nodesCount; i++) {
